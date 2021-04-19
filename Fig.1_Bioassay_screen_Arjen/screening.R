df8cd571cf73d711726a99c4b26fe9758c7da5e9
Marc Galland <m.galland@uva.nl>
2021-04-19 17:59:30 +0200

    add suggestions to the screening script

--- Fig.1_Bioassay_screen_Arjen/screening.R
+++ Fig.1_Bioassay_screen_Arjen/screening.R
@@ -1,53 +1,65 @@
+library("checkpoint")
+checkpoint("2020-12-01")
 
-Screen <- read.csv("Fig.1_Bioassay_screen_Arjen/hertelling.csv", 
-                   header = T, 
-                   sep = ";")
-
-#Percentage<-read.csv("hertelling_Arjens_screen.csv", header=T, sep = ";", dec = ",")
-
-checkpoint::checkpoint("2020-12-01")
+#############
+# Library
+#############
 library(ggpubr)
 library(multcomp)
 library(car)
 library(tidyverse)
-#library(rstatix)
+library(rstatix) # pipe friendly statistical tests
 
 
-## Make sure MM is the first genotype
-Screen[1:4,"genotype"] <- "AMM"
-Screen[24:27,"genotype"] <- "AMM"
+#############
+# Data import
+#############
+screening_data <- read.csv("Fig.1_Bioassay_screen_Arjen/hertelling.csv", 
+                   header = T, 
+                   stringsAsFactors = FALSE) # ensures that you do the conversion yourself
 
-##Make Screen$genotype into a factor
-Screen$genotype = as.factor(Screen$genotype)
-Screen$stage = as.factor(Screen$stage)
 
+# Genotype and stage as factors
+screening_data <- screening_data %>% 
+  mutate(genotype = factor(genotype, levels = unique(genotype))) %>% 
+  mutate(stage = factor(stage, levels = c("egg", "fourth+exuviae")))
+         
+
+
+###########################
+## plot for eggs and nymphs
+###########################
+egg_nymph_plot <- ggplot(screening_data, aes(x=stage, y=number, fill=stage)) + 
+  labs(y = "# of individuals") +
+  geom_boxplot() + 
+  geom_point(aes(fill = stage), size = 1, shape = 21,
+             position = position_jitterdodge(jitter.width = 0.1)) +
+  theme_classic() +
+  facet_wrap(~ genotype, nrow = 2)
+egg_nymph_plot
+
+ggsave(filename = "Fig.1_Bioassay_screen_Arjen/plot_eggs_nymphs.png", 
+       plot = egg_nymph_plot)
+
+
+##############
+## ANOVA tests
+##############
 
 ## statistics eggs
-eggs <- aov(number~genotype, data = Screen[which(Screen$stage=='egg'), ])
-summary(eggs)
 
-shapiro.test(resid(eggs))
-leveneTest(eggs)
+screening_data %>% 
+  select(- replicate) %>% 
+  nest(data = c(genotype, number)) %>% 
+  mutate(aov_test = map(data, ~ aov(number ~ genotype, data = .x))) %>% 
+  mutate(tidy_aov_test = map(aov_test, tidy)) %>% 
+  unnest(tidy_aov_test) %>% 
+  filter(term == "genotype")
+  
 
-test_eggs <- glht(eggs, linfct=mcp(genotype="Dunnett"))
-summary(test_eggs)
-
-## statistics nymphs
-nymphs <- aov(number~genotype, data = Screen[which(Screen$stage=='fourth+exuviae'), ])
-summary(nymphs)
-
-shapiro.test(resid(nymphs))
-leveneTest(nymphs)
-
-test_nymphs <- glht(nymphs, linfct=mcp(genotype="Dunnett"))
-summary(test_nymphs)
-
-
-##remove empty rows
-Screen <- Screen[-c(47:73),]
 
 ## paired t-test per genotype
-stat.test <- Screen %>%
+stat.test <- screening_data %>%
   group_by(genotype) %>%
   pairwise_t_test(
     number ~ stage, paired = TRUE, 
@@ -58,15 +70,4 @@ stat.test
 
 
 
-library(ggplot2)
-
-##plot for eggs and nymphs
-egg_nymph_plot <- ggplot(Screen, aes(x=genotype, y=number, fill=stage)) + 
-  labs(y = "# of individuals") +
-  geom_boxplot() + 
-  geom_point(aes(fill = stage), size = 1, shape = 21,
-             position = position_jitterdodge()) +
-  theme_classic()
-  
-egg_nymph_plot
 
