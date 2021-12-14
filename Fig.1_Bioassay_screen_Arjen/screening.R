4ca6197965a5effe181cbfb12a72c6ee328182c1
LissyDenkers <l.a.m.denkers@uva.nl>
2021-12-14 17:38:13 +0100

    included the percentage of eggs to nymphs

--- Fig.1_Bioassay_screen_Arjen/screening.R
+++ Fig.1_Bioassay_screen_Arjen/screening.R
@@ -14,7 +14,7 @@ library(rstatix) # pipe friendly statistical tests
 #############
 # Data import
 #############
-screening_data <- read.csv("hertelling.csv", 
+screening_data <- read.csv("Fig.1_Bioassay_screen_Arjen/hertelling.csv", 
                    header = T, 
                    stringsAsFactors = FALSE) # ensures that you do the conversion yourself
 
@@ -22,24 +22,30 @@ screening_data <- read.csv("hertelling.csv",
 # Genotype and stage as factors and filter genotype
 screening_data <- screening_data %>% 
   mutate(genotype = factor(genotype, levels = unique(genotype))) %>% 
-  mutate(stage = factor(stage, levels = c("egg", "fourth+exuviae"))) %>%
+  mutate(stage = factor(stage, levels = c("egg", "fourth_instar"))) %>%
   filter(genotype == "Moneymaker" | genotype == "LA1840")
          
 
+# Add percentage of fourth instars per eggs
+screening_data <- screening_data %>% 
+  pivot_wider(names_from = stage, values_from = number) %>%
+  mutate(percentage_from_eggs = fourth_instar / egg * 100) %>%
+  pivot_longer(cols = fourth_instar:percentage_from_eggs, 
+               names_to = "stage", values_to = "number")
 
 ###########################
 ## plot for eggs and nymphs
 ###########################
-egg_nymph_plot <- ggplot(screening_data, aes(x=stage, y=number, fill=stage)) + 
+egg_nymph_plot <- ggplot(screening_data, aes(x=genotype, y=number, fill=genotype)) + 
   labs(y = "# of individuals") +
   geom_boxplot() + 
-  geom_point(aes(fill = stage), size = 1, shape = 21,
+  geom_point(aes(fill = genotype), size = 1, shape = 21,
              position = position_jitterdodge(jitter.width = 0.1)) +
   theme_classic() +
-  facet_wrap(~ genotype)
+  facet_wrap(~ stage, scales = "free_y")
 egg_nymph_plot
 
-ggsave(filename = "Fig.1_Bioassay_screen_Arjen/plot_eggs_nymphs.png", 
+ggsave(filename = "Fig.1_Bioassay_screen_Arjen/plot_eggs_nymphs2.png", 
        plot = egg_nymph_plot)
 
 
